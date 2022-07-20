82d62a23c383b441acdb52bccaf533d0b3e22715
이수연 <81813324+coding-groot@users.noreply.github.com>
2022-07-20 11:25:22 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,10 +1,6 @@
 import math
 def solution(progresses, speeds):
-    padding = [0]
-    left_date = [math.ceil((100-progresses[i])/speeds[i]) for i in range(len(progresses))]
-    left_date = padding+left_date
-    new = [i for i in range(1,len(left_date)) if left_date[i]>max(left_date[:i])]
-    new.append(len(left_date))
+    left_date = [0] + [math.ceil((100-progresses[i])/speeds[i]) for i in range(len(progresses))]
+    new = [i for i in range(1,len(left_date)) if left_date[i]>max(left_date[:i])]+[len(left_date)]
     answer = [new[i]-new[i-1] for i in range(1,len(new))]
-    
     return answer
