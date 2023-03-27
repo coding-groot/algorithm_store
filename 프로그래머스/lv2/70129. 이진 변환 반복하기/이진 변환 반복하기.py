0ebc7c1037e725ec2fb67934560175022f737041
이수연 <81813324+coding-groot@users.noreply.github.com>
2023-03-27 17:20:49 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.97 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -4,12 +4,9 @@ def func(s):
 def solution(s):
     zero_cnt = 0
     cnt = 0
-    one_cnt = 0
-    while one_cnt!=1:
+    while s!='1':
         zero_cnt += func(s)[0]
         cnt +=1
         s= func(s)[1]
-        if s=='1':
-            one_cnt +=1
     answer = [cnt,zero_cnt]
     return answer
